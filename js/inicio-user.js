7df2ccd50b7cbdce9130874f4c9fda50941462b4
FrancoMinelli11 <francominelli11@gmail.com>
2024-06-30 16:33:25 -0300

    Creación de páginas de registro y sus respectivos JS

---
+++ js/inicio-user.js
@@ -0,0 +1,25 @@
+const FORM_INICIAR_SESION = document.getElementById('form-iniciar-sesion');
+const INPUT_USUARIO = document.getElementById('user-name');
+const INPUT_CONTRASEÑA = document.getElementById('password');
+
+FORM_INICIAR_SESION.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const NOMBRE_DE_USUARIO = INPUT_USUARIO.value;
+    const CONTRASEÑA = INPUT_CONTRASEÑA.value;
+
+    if (NOMBRE_DE_USUARIO !== "" && CONTRASEÑA !== "") {
+
+        localStorage.setItem('NOMBRE_DE_USUARIO', NOMBRE_DE_USUARIO);
+        localStorage.setItem('CONTRASEÑA', CONTRASEÑA);
+
+        if (NOMBRE_DE_USUARIO === "Francol" && CONTRASEÑA === "nashex") {
+            localStorage.setItem('ES_ADMIN', 'true');
+        } else {
+            localStorage.setItem('ES_ADMIN', 'false');
+        }
+
+        window.location.href = "../index.html";
+    } else {
+        alert('Por favor, complete todos los campos.');
+    }
+});
--- js/registro_inicio.js
+++ js/registro_inicio.js
@@ -1,13 +1,25 @@
 //Llamado de elementos
 const REGISTRO_INICIO = document.getElementById('registro-inicio')
+const NAVBAR = document.getElementById('barra-nav')
 
-//Funciones
-
-REGISTRO_INICIO.addEventListener('click', () => {
-    swal.fire({
-        title: 'Bienvenido a KayFs, ingresa para comprar con tus datos más facilmente',
-        html:`<button class="boton-popup mb-2" id="registro">Registrarse</button> <br>
-              <button class="boton-popup" id="inicio">Iniciar sesión</button>`,
-        showConfirmButton: false,
-    })
+// Funciones
+REGISTRO_INICIO.addEventListener('click', () =>{
+    window.location.href = "../pages/registro.html"
 })
+
+window.onload = function() {
+    const NOMBRE_DE_USUARIO = localStorage.getItem('NOMBRE_DE_USUARIO');
+    const CONTRASEÑA = localStorage.getItem('CONTRASEÑA');
+    const ES_ADMIN = localStorage.getItem('ES_ADMIN');
+
+    if (NOMBRE_DE_USUARIO && CONTRASEÑA) {
+        console.log('Nombre de usuario:', NOMBRE_DE_USUARIO);
+        console.log('Contraseña:', CONTRASEÑA); 
+        if (ES_ADMIN === 'true' && NAVBAR) {
+            const ELEMENTO_LISTA = document.createElement('li');
+            ELEMENTO_LISTA.className = 'nav-item';
+            ELEMENTO_LISTA.innerHTML = `<a class="nav-link barra-n" href="./Pages/admin.html">Administradores</a>`;
+            NAVBAR.appendChild(ELEMENTO_LISTA);
+        }
+    }
+};
