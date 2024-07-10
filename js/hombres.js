f0eaed59b4c4b18fa91e26bc550787c40112c5da
FrancoMinelli11 <francominelli11@gmail.com>
2024-07-10 12:22:59 -0300

    Creado de articulos.json y Mejorado de funciones con JS

--- js/hombres.js
+++ js/hombres.js
@@ -5,9 +5,38 @@ const DIV_HOMBRES = document.getElementById('div-hombres-prendas');
 // Función para agregar productos al DOM
 
 function renderizarProductos() {
-    //Traer productos del localStorage
-    const productos = JSON.parse(localStorage.getItem('hombres.html')) || [];
+    //Llamar al archivo .json
+    const ART = "../articulos.JSON"
+    
+    //Promesa para crear los artículos del .json
+    fetch(ART)
+    .then(respuesta => respuesta.json())
+    .then((datos) => {
+        cargarArti(datos)
+    })
+    .catch((error) => console.log(error))
+    .finally(() => console.log("Elementos creados"))
+    
+    //Función para crear los artículos del .json
+    function cargarArti (datos){
+        datos.hombres.forEach(arti => {
+            const CARD_H = document.createElement('div');
+            CARD_H.className = 'card card-look';
+            CARD_H.innerHTML = `
+            <img src="${arti.foto}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${arti.nombre}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">$${arti.precio}</h6>
+            <p class="card-text">${arti.descripcion}</p>
+            <a href="#" class="btn btn-look">Comprar ahora</a>
+            </div>`;
+            DIV_HOMBRES.appendChild(CARD_H);
+        })
+    }
 
+    //Traer productos del localStorage de elementos creados dentro de la web
+    const productos = JSON.parse(localStorage.getItem('hombres.html')) || [];
+    
     //Crear el producto en HTML
     productos.forEach(producto => {
         const CARD = document.createElement('div');
@@ -23,6 +52,5 @@ function renderizarProductos() {
         DIV_HOMBRES.appendChild(CARD);
     });
 }
-
 
 document.addEventListener('DOMContentLoaded', renderizarProductos);
--- js/mujeres.js
+++ js/mujeres.js
@@ -5,6 +5,35 @@ const DIV_MUJERES = document.getElementById('div-mujeres-prendas');
 // Función para agregar productos al DOM
 
 function renderizarProductos() {
+    //Llamar al archivo .json
+    const ART = "../articulos.JSON"
+    
+    //Promesa para crear los artículos del .json
+    fetch(ART)
+    .then(respuesta => respuesta.json())
+    .then((datos) => {
+        cargarArti(datos)
+    })
+    .catch((error) => console.log(error))
+    .finally(() => console.log("Elementos creados"))
+    
+    //Función para crear los artículos del .json
+    function cargarArti (datos){
+        datos.mujeres.forEach(arti => {
+            const CARD_M = document.createElement('div');
+            CARD_M.className = 'card card-look';
+            CARD_M.innerHTML = `
+            <img src="${arti.foto}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${arti.nombre}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">$${arti.precio}</h6>
+            <p class="card-text">${arti.descripcion}</p>
+            <a href="#" class="btn btn-look">Comprar ahora</a>
+            </div>`;
+            DIV_MUJERES.appendChild(CARD_M);
+        })
+    }
+
     //Traer productos del localStorage
     const productos = JSON.parse(localStorage.getItem('mujeres.html')) || [];
 
